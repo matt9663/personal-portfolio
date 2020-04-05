2b8dc4b21521dc76cb14c264a52ec8a4a9d176df
Matt Wood <matt9663@gmail.com>
2020-04-05 13:59:26 -0400

    added slide-in animation for project cards

--- index.js
+++ index.js
@@ -11,4 +11,34 @@ function sendMeAnEmail() {
     });
 }
 
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if(!immediate) func.apply(context, args)
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args)
+  };
+};
+
+
+function checkScroll(e) {
+  const cards = document.querySelectorAll('.slide-in');
+  cards.forEach(card => {
+    const slideInAt = (window.scrollY + window.innerHeight) - (card.offsetHeight * .10);
+    const isQuarterShown = slideInAt > card.offsetTop;
+
+    if (isQuarterShown) {
+      card.classList.add('active')
+    }
+  });
+}
+
+$(window).on('scroll', debounce(checkScroll, 80));
+
 $(sendMeAnEmail);
